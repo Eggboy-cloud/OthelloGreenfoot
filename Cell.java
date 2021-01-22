91526e8f4780dcf2d4a844fa3d57f09f4cb9723c
Egil Guting <egil.guting@skola.taby.se>
2021-01-22 08:58:04 +0100

    version 1

---
+++ Cell.java
@@ -0,0 +1,22 @@
+import greenfoot.*;
+
+public class Cell extends Actor
+{
+    MouseInfo mouse = Greenfoot.getMouseInfo();
+    OthelloWorld world = (OthelloWorld)getWorld();
+    
+    public int x;
+    public int y;
+    
+    public Cell(int x,int y)
+    {
+        x = this.x;
+        y = this.y;
+    }
+    
+    
+    public void act() 
+    {
+        
+    }
+}
---
+++ OthelloWorld.java
@@ -0,0 +1,46 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;  // Java library
+/**
+ * Contains the Code for the Gameboard
+ * 
+ * @Egil Guting 
+ * @2021-18-01
+ */
+public class OthelloWorld extends World
+{
+    MouseInfo mouse = Greenfoot.getMouseInfo();
+    
+    //Values used when building the Greenfoot World and when placing out the empty cells
+    public static final int STANDARD_SIDE_LENGTH_ = 8;
+    public static final int IMAGE_LENGTH_ = 55;
+    public static final int VERTICAL_MARGIN_ = 20;
+    public static final int HORIZONTAL_MARGIN_ = 20;
+    
+    private Cell[][] cellList = new Cell[8][8]; //Contains all positions on the board, empty, black or white.
+    public OthelloWorld()
+    {  
+        super(STANDARD_SIDE_LENGTH_ * IMAGE_LENGTH_ + HORIZONTAL_MARGIN_ * 2, STANDARD_SIDE_LENGTH_ * IMAGE_LENGTH_ + VERTICAL_MARGIN_ * 2, 1);
+        constructBoard();
+    }
+    
+    public void act()
+    {
+        
+    }
+    
+    public void constructBoard()
+    {
+        //Places out empty cells which will be played upon
+        int xStartLocation = HORIZONTAL_MARGIN_ + IMAGE_LENGTH_/2;
+        int yStartLocation = VERTICAL_MARGIN_ + IMAGE_LENGTH_/2;
+        
+        for (int x = 0; x<=7;x++)
+            {
+                for(int y = 0; y<=7;y++)
+                {
+                    cellList[x][y] = new Cell(x,y);
+                    addObject(cellList[x][y], x * IMAGE_LENGTH_ + xStartLocation, y * IMAGE_LENGTH_ + yStartLocation);
+                }
+            }
+    }
+}
