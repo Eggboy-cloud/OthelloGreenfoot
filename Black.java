c895f39a70dab0dfeb13d11dcffb32f79ba56446
Egil Guting <egil.guting@skola.taby.se>
2021-02-01 14:13:06 +0100

    Grafikproblem

---
+++ Black.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class Black here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Black extends Cell
+{    
+
+}
--- Cell.java
+++ Cell.java
@@ -1,22 +1,36 @@
-import greenfoot.*;
-
-public class Cell extends Actor
-{
-    MouseInfo mouse = Greenfoot.getMouseInfo();
-    OthelloWorld world = (OthelloWorld)getWorld();
+    import greenfoot.*;
     
-    public int x;
-    public int y;
-    
+    public class Cell extends Actor
+    {
+        
+        public int x;
+        public int y;
+    //    GreenfootImage black = new GreenfootImage("black.gif");
+    //    GreenfootImage image = black.getImages();
+    //    image.scale(30,30);
+        
     public Cell(int x,int y)
     {
         x = this.x;
         y = this.y;
     }
     
-    
     public void act() 
     {
-        
+    
+    }
+    
+    public void setBlack()
+    {
+        GreenfootImage black = new GreenfootImage("black.gif");
+        black.scale(50,50);
+        setImage(black);
+    }
+    
+    public void setWhite()
+    {
+        GreenfootImage white = new GreenfootImage("white.png");
+        white.scale(50,50);
+        setImage(white);
     }
 }
