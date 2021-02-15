21ce3a749b4f00ad967fdcaafc04a9b7eb49e162
Linus <linus.sjodin@skola.taby.se>
2021-02-15 13:44:35 +0100

    clicking on cell adds piece

--- Cell.java
+++ Cell.java
@@ -3,7 +3,7 @@
     {
       public int x; // X cordinate
       public int y; // y cordinate
-        
+      OthelloWorld world = (OthelloWorld)getWorld();  
     public Cell(int x,int y)
     {
         x = this.x;
@@ -12,12 +12,15 @@
     
     public void act() 
     {
-    
+        if(Greenfoot.mouseClicked(this)){
+            System.out.println("X Coord: " + (getX()-47)/55 + " Y Coord: " +(getY()-47)/55);
+            world.play( ((getX()-47)/55),((getY()-47)/55),true);
+        }
     }
     
     public void setBlack()
     {
-        GreenfootImage black = new GreenfootImage("black.gif");
+        GreenfootImage black = new GreenfootImage("black.png");
         black.scale(50,50);
         setImage(black);
     }
--- OthelloWorld.java
+++ OthelloWorld.java
@@ -1,25 +1,20 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 import java.util.*;  // Java library
-/**
- * Contains the Code for the Gameboard
- * 
- * @Egil Guting 
- * @2021-18-01
- */
+
 public class OthelloWorld extends World
 {
     MouseInfo mouse = Greenfoot.getMouseInfo();
     
     //Values used when building the Greenfoot World and when placing out the empty cells
-    public static final int STANDARD_SIDE_LENGTH_ = 8;
-    public static final int IMAGE_LENGTH_ = 55;
-    public static final int VERTICAL_MARGIN_ = 20;
-    public static final int HORIZONTAL_MARGIN_ = 20;
+    public static final int StandardSideLength = 8;
+    public static final int ImageLength = 55;
+    public static final int VerticalMargin = 20;
+    public static final int HorizontalMargin = 20;
     
     private Cell[][] cellList = new Cell[8][8]; //Contains all positions on the board, empty, black or white.
     public OthelloWorld()
     {  
-        super(STANDARD_SIDE_LENGTH_ * IMAGE_LENGTH_ + HORIZONTAL_MARGIN_ * 2, STANDARD_SIDE_LENGTH_ * IMAGE_LENGTH_ + VERTICAL_MARGIN_ * 2, 1);
+        super(StandardSideLength * ImageLength + HorizontalMargin * 2, StandardSideLength * ImageLength + VerticalMargin * 2, 1);
         constructBoard();
     }
     
@@ -31,15 +26,15 @@ public class OthelloWorld extends World
     public void constructBoard()
     {
         //Places out empty cells which will be played upon
-        int xStartLocation = HORIZONTAL_MARGIN_ + IMAGE_LENGTH_/2;
-        int yStartLocation = VERTICAL_MARGIN_ + IMAGE_LENGTH_/2;
+        int xStartLocation = HorizontalMargin + ImageLength/2;
+        int yStartLocation = VerticalMargin + ImageLength/2;
         
         for (int x = 0; x<=7;x++)
             {
                 for(int y = 0; y<=7;y++)
                 {
                     cellList[x][y] = new Cell(x,y);
-                    addObject(cellList[x][y], x * IMAGE_LENGTH_ + xStartLocation, y * IMAGE_LENGTH_ + yStartLocation);
+                    addObject(cellList[x][y], x * ImageLength + xStartLocation, y * ImageLength + yStartLocation);
                 }
             }
             
@@ -48,5 +43,14 @@ public class OthelloWorld extends World
         cellList[4][4].setBlack();
         cellList[4][3].setWhite();  
         
+        
+    }
+    
+    public void play(int x, int y,boolean color)
+    {
+        if (color == true)
+            cellList[x][y].setBlack();
+        else 
+            cellList[x][y].setWhite();
     }
 }
